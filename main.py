019ab49af03b705e42dd0fd4f2bff3c194c4006d
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-01 11:52:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#PROGRAM: Main Execution script
+#Description: Imports the custom 'spam' module to call its functions and access its variables.
+import spam #Imports the code from spam.py
+
+spam.bacon() # call the bacon function defined inside the spam module
+print(spam.word) # Print the value of the 'word' variable defined inside the spam module
