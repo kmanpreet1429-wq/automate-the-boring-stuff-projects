afa118cfc619d318f279574c7fdeaac649429e55
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-02 14:40:09 +0530

    Add files via upload

---
+++ string_length_printer.py
@@ -0,0 +1,6 @@
+# A simple functio  that accepts a string as an argument, calculates its length
+# and prints the numeric length alongside the original string.
+def me(you):
+    length = len(you)
+    print(str(length) + ' ' + you)
+me('hy')
