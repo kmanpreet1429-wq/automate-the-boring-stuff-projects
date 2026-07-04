800d2a63c51ac014ed1f787c25de5a54c6beb086
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:43:25 +0530

    basic_login_system.py

--- basic_login_system.py
+++ basic_login_system.py
@@ -1,19 +1,9 @@
-#PROGRAM: Basic Login Gatekeeper
-# DESCRIPTION: A simple dual-verification authentication script practicing loop
-#              control mechanics. It uses 'continue' to restart the loop if the
-#              username is incorrect, and 'break' to escape the infinite loop
-#              entirely once both credentials match.
-# ==============================================================================
-while True: #Starts an infinite loop for the login prompt
-
-    name = input('Who are you?') #Asks for a username input
-
-    if name!= 'Joe': #It checks if the input taken by user is 'Joe' or NOT
-        continue #If it is not Joe, it jumps the top of the loop again and takes input
-
-    print('Hello joe, what is you password?') # This part will be executed only in condition if the name typed by user is 'Joe'
-
-    password = input() #Takes input for password
-    if password == 'swan': # This statement checks if the password is correct
-        break # If the password is correct it will just break the statement
-print('Got it!') # After breaking the statement succesfully it will print this
+while True: 
+    name = input('Who are you?') 
+    if name!= 'Joe':
+        continue 
+    print('Hello joe, what is you password?') 
+    password = input() 
+    if password == 'swan':
+        break 
+print('Got it!') 
