6c222502fd92826548d4f3c2c337459b1e989195
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:58:37 +0530

    while_loop_counter.py

--- while_loop_counter.py
+++ while_loop_counter.py
@@ -1,8 +1,5 @@
-# Program: While Loop Counter Practice
-# Description: Uses a while loop and an f-string counter to print a string 5 times.
-
 print('I am')
-i = 0 # Initialize the counter variable at 0
-while i < 5: # It will keep running as long as i is less than 5
+i = 0 
+while i < 5:
     print(f'happy({i})')
-    i = i + 1 # increaments the counter by 1 and avoide an infinite loop
+    i = i + 1 
