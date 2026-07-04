8e3d2974d45538da0cf15812558dc4eb53f85a6d
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:54:17 +0530

    sum_first_50_numbers.py

--- sum_first_50_numbers.py
+++ sum_first_50_numbers.py
@@ -1,8 +1,5 @@
-#PROGRAM: Sum of first 50 Numbers (0 to 49)
-#Description: Uses a for loop to calculate total sum.
-
-total = 0 #Initialize with a varibale with sum 0
-for i in range(50): # i counts from 0 to 49
-    total = total + i # Adds current value to the total sum present at that time
+total = 0 
+for i in range(50): 
+    total = total + i 
 
 print(total) # Displays the final calculated sum
