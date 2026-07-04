6e6fd285d3d72d531b270d455dacb1e59eb42341
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:44:09 +0530

    generate_random_numbers.py

--- generate_random_numbers.py
+++ generate_random_numbers.py
@@ -1,7 +1,3 @@
-# Program: Generate Random Numbers
-# Description: Uses the random module and a for loop to print 5 random integers.
-
-import random #Import Python's built-in random module to access randomizing functions
-
-for i in range(5): # The variable i counts from 0 up to 4
-    print(random.randint(1,10)) #Generate and prints any 5 random integers between 1 to 10
+import random 
+for i in range(5): 
+    print(random.randint(1,10)) 
