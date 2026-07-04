894f9b65c40d1581e671234b301f0fa546136486
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:52:56 +0530

    spam.py

--- spam.py
+++ spam.py
@@ -1,8 +1,5 @@
-#PROGRAM: Custom Spam Module
-#Description: A module containing a greeting function and a message variable designed to be imported and used by other Python scripts.
 def bacon():
     print('hy')
-# A module level variable storing a string
 word = 'Love'
 
 
