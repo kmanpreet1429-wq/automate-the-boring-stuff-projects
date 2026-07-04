5aed415318f87abe1164187e511c98c01f940d76
kmanpreet1429-wq <k.manpreet.1429@gmail.com>
2026-07-04 09:45:12 +0530

    guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,22 +1,17 @@
-#PROGRAM: Guess the  Number game
-#Description: A game where a player tries to guess a random number in 6 tries.
-
-import random # Import the random module to generate a secret number
-
-secretNumber = random.randint(1,20) #Generate a random target integer between 1 and 20
+import random 
+secretNumber = random.randint(1,20) 
 print('I am thinking of a number between the range of 1 and 20')
 
-for noOfGuesses in range(1,7): # Allow the player up to 6 guesses
+for noOfGuesses in range(1,7): 
     print('take a guess')
     guess = int(input())
 
-    if guess < secretNumber: # Checks if the guess is too low
+    if guess < secretNumber: 
         print('higher')
-    elif guess > secretNumber: # Checks if the guess is too high
+    elif guess > secretNumber: 
         print('lower')
-    else: # If the guess is correct, Break out of the loop early
+    else: 
         break
-
 if guess == secretNumber:
     print('you guessed the number in ' + str(noOfGuesses) + ' guesses.')
 else:
